f87258d9e3c06a9be559efa31fa9ee991a84c6bd
Wangsherpa <wangdoje@gmail.com>
2021-12-23 10:04:31 +0530

    added argument parser

--- convert.py
+++ convert.py
@@ -1,8 +1,16 @@
 import argparse
 from converter import Converter
 
-json_data = Converter.excel2json('data.xlsx', 'Marks',
-                                 output_filename="new_output.jxon",
-                                 orient='columns')
+parser = argparse.ArgumentParser(description="convert excel to json format")
+parser.add_argument('--source_path', type=str, required=True, help="path to excel file")
+parser.add_argument('--sheet_name', type=str, required=True, help="name of the sheet")
+parser.add_argument('--target_path', type=str, required=False, help="path+name of the target json file")
+parser.add_argument('--orient', type=str, required=False, default='records', help="orient type: index, records or columns")
+
+args = parser.parse_args()
+
+json_data = Converter.excel2json(args.source_path, args.sheet_name,
+                                 output_filename=args.target_path,
+                                 orient=args.orient)
 
 print(json_data)
